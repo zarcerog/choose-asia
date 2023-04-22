1bacaa83fa8044d6ede0bbc1e8594f1fd876c89f
zarcerog <nicostios24@gmail.com>
2023-04-22 21:40:51 +0200

    feat: :sparkles: add 404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
+            <h1 className="text-4xl font-bold">404</h1>
+            <p className="text-xl">Page not found</p>
+        </div>
+    );
+};
+
+export default NotFound;
